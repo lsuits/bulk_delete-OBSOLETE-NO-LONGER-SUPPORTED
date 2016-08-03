0f5f061658eda1c72d03639bb260ccdab79ef378
Robert Russo <rrusso@lsu.edu>
2016-08-03 09:43:30 -0500

    Updated for 3.x

--- index.php
+++ index.php
@@ -34,13 +34,23 @@ $form = new bulk_delete_form();
 if ($form->is_cancelled()) {
     redirect(new moodle_url('/blocks/bulk_delete/'));
 } else if ($data = $form->get_data()) {
+    $i = 0;
     foreach (explode("\n", $data->courses) as $shortname) {
         $shortname = trim($shortname);
 
         $course = $DB->get_record('course', array('shortname' => $shortname));
 
         if ($course) {
+            $i++;
+            $infostart= '<center><strong>Deleting ' . $course->fullname . ' &mdash; # ' . $i . '</strong></center>';
+            mtrace($infostart, $eol="\n", $sleep=0);
+            $starttime = microtime(true);
             delete_course($course, false);
+            $endtime = microtime(true);
+            $timediff = $endtime - $starttime;
+            $timediff = round($timediff);
+            $infocomplete = '<center><strong>' . $course->fullname . ' Deleted &mdash; # ' . $i . ' &mdash; Deletion took ' . $timediff . ' seconds.</strong></center><br />';
+            mtrace($infocomplete, $eol="\n", $sleep=0);
         }
     }
 
